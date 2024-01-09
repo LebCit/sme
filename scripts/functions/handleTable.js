b8222bbede56855aff36b619badf85ae53b9cd73
LebCit <lebcit@gmail.com>
2024-01-10 01:59:20 +0200

    first commit

---
+++ scripts/functions/handleTable.js
@@ -0,0 +1,68 @@
+import { manageTable } from "./manageTable.js"
+import { prettyPrint } from "./mdTablePrettyPrint.js"
+import { convertTableToMarkdown } from "./convertTableToMarkdown.js"
+
+export const handleTable = async (editor, handleInput) => {
+	const selectionStart = editor.selectionStart
+	const selectionEnd = editor.selectionEnd
+
+	const { value: tableValues } = await Swal.fire({
+		title: "Create a table",
+		html: `
+			<sme-table data-sm="1column gap">
+                <sme-table-actions data-sm="4column">
+                    <button data-sm="center" id="add-column" class="sme-md-btn" title="Add Column"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-column-insert-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><title>Add Column</title><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1z" /><path d="M15 12l4 0" /><path d="M17 10l0 4" /></svg></button>
+                    <button data-sm="center" id="remove-column" class="sme-md-btn" title="Remove Column"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-column-remove" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><title>Remove Column</title><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1z" /><path d="M16 10l4 4" /><path d="M16 14l4 -4" /></svg></button>
+                    <button data-sm="center" id="add-row" class="sme-md-btn" title="Add Row"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-row-insert-bottom" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><title>Add Row</title><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 6v4a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h14a1 1 0 0 1 1 1z" /><path d="M12 15l0 4" /><path d="M14 17l-4 0" /></svg></button>
+                    <button data-sm="center" id="remove-row" class="sme-md-btn" title="Remove Row"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-row-remove" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><title>Remove Row</title><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 6v4a1 1 0 0 1 -1 1h-14a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h14a1 1 0 0 1 1 1z" /><path d="M10 16l4 4" /><path d="M10 20l4 -4" /></svg></button>
+                </sme-table-actions>
+
+                <sme-table-container data-sm="center">
+                    <table id="sme-table-element" tabindex=0 role="grid">
+                        <thead>
+                            <tr></tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </sme-table-container>
+            </sme-table>
+		`,
+		grow: "row", // popup should grow to fill the available width.
+		allowOutsideClick: false, // user can't dismiss the popup by clicking outside it.
+		showCloseButton: true,
+		focusConfirm: false, // focus the first element in tab order instead of "Confirm"-button by default.
+		returnFocus: false, // don't return the focus to the element that invoked the modal after the modal is closed.
+		didOpen: () => {
+			// Call the function to initialize event listeners
+			manageTable("sme-table-element")
+		},
+		preConfirm: () => {
+			return convertTableToMarkdown("sme-table-element")
+		},
+		confirmButtonColor: "var(--sme-gray-9)",
+		confirmButtonText: "Insert table",
+	})
+
+	if (tableValues) {
+		const markdownTableSeparator = "\n***\n" // <hr />
+
+		// Update editor value
+		editor.value =
+			editor.value.substring(0, selectionStart) +
+			prettyPrint(tableValues) +
+			markdownTableSeparator +
+			editor.value.substring(selectionEnd)
+
+		// Calculate the new cursor position
+		const newCursorPosition = selectionStart + prettyPrint(tableValues).length + markdownTableSeparator.length
+
+		// Set cursor position after insertion
+		editor.setSelectionRange(newCursorPosition, newCursorPosition)
+
+		// Focus in the editor at the current position
+		editor.focus()
+
+		// Update preview after modification
+		handleInput()
+	}
+}
---
+++ scripts/functions/initializeMarkdownPreview.js
@@ -0,0 +1,130 @@
+import UndoManager from "https://cdn.jsdelivr.net/npm/undo-manager@1.1.1/+esm"
+
+// Function to set up the Markdown preview functionality
+export const initializeMarkdownPreview = (editor, preview) => {
+	// Initialize UndoManager
+	const undoManager = new UndoManager()
+	undoManager.setLimit(20) // Set a limit of 20 undo steps
+
+	// Get references to HTML elements
+	const undoBtn = document.getElementById("undoBtn")
+	const redoBtn = document.getElementById("redoBtn")
+	const htmlBtn = document.getElementById("html-preview")
+	const markdownBtn = document.getElementById("markdown-preview")
+
+	// Function to update the buttons' disabled state based on undo/redo availability
+	const updateButtons = () => {
+		undoBtn.disabled = !undoManager.hasUndo()
+		redoBtn.disabled = !undoManager.hasRedo()
+	}
+
+	// Function to update the preview
+	const updatePreview = (markdownContent) => {
+		try {
+			preview.innerHTML = marked.parse(markdownContent)
+		} catch (error) {
+			console.error("Error parsing Markdown:", error.message)
+			// Handle the error (e.g., display a user-friendly message in the UI)
+		}
+	}
+
+	// Functions to handle preview switching
+
+	// Function to show the raw HTML of the preview
+	const showHtmlPreview = () => {
+		const parsedHtml = marked.parse(editor.value) // Get the HTML from the Markdown content
+		preview.innerText = parsedHtml // Set the preview to display the raw HTML
+		htmlBtn.closest("sme-dropdown").firstElementChild.checked = false
+		// Optionally, update button states or styling to indicate the current preview mode
+	}
+
+	// Function to show the parsed Markdown to HTML
+	const showMarkdownPreview = () => {
+		preview.innerHTML = marked.parse(editor.value) // Parse the Markdown again to ensure fresh rendering
+		markdownBtn.closest("sme-dropdown").firstElementChild.checked = false
+		// Optionally, update button states or styling to indicate the current preview mode
+	}
+
+	// Add event listeners to the buttons preview
+	htmlBtn.addEventListener("click", showHtmlPreview)
+	markdownBtn.addEventListener("click", showMarkdownPreview)
+
+	// Function to handle user input
+	const handleInput = () => {
+		const markdownContent = editor.value
+
+		undoManager.add({
+			value: markdownContent,
+			undo: function () {
+				editor.value = this.value // Undo changes
+				updatePreview(editor.value) // Update the preview
+			},
+			redo: function () {
+				editor.value = this.value // Redo changes
+				updatePreview(editor.value) // Update the preview
+			},
+		})
+
+		updatePreview(markdownContent)
+
+		updateButtons()
+	}
+
+	// Function to handle undo button click
+	function undo() {
+		if (undoManager.hasUndo()) {
+			undoManager.undo()
+			const commands = undoManager.getCommands()
+			if (commands.length > 0) {
+				const index = undoManager.getIndex()
+				const undoContent = commands[index] ? commands[index].value : ""
+				editor.value = undoContent
+				updatePreview(undoContent) // Update the preview with the content from the undo command
+			} else {
+				editor.value = ""
+				updatePreview("") // Update the preview since the editor is empty
+			}
+		}
+		updateButtons()
+	}
+
+	// Function to handle redo button click
+	function redo() {
+		if (undoManager.hasRedo()) {
+			undoManager.redo()
+			const commands = undoManager.getCommands()
+			const index = undoManager.getIndex()
+			if (commands[index]) {
+				editor.value = commands[index].value
+			}
+		}
+		updateButtons()
+	}
+
+	// Attach event listeners to buttons
+	undoBtn.addEventListener("click", undo)
+	redoBtn.addEventListener("click", redo)
+
+	// Event listener for editor changes
+	editor.addEventListener("input", handleInput)
+
+	// Event listener for Ctrl+Z and Ctrl+y (undo and redo shortcuts)
+	document.addEventListener("keydown", function (event) {
+		if (event.ctrlKey) {
+			if (event.key === "z") {
+				undo()
+				event.preventDefault() // Prevent the default browser behavior for Ctrl+Z and Ctrl+Y
+			} else if (event.key === "y") {
+				redo()
+				event.preventDefault() // Prevent the default browser behavior for Ctrl+Z and Ctrl+Y
+			}
+			//event.preventDefault() // Prevent the default browser behavior for Ctrl+Z and Ctrl+Y
+		}
+	})
+
+	// Initial preview
+	handleInput()
+
+	// Return handleInput
+	return handleInput
+}
